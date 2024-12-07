1697469401003ddba2d276784204aa41f14ea4e3
tooOshiroo0o <rbbnyy@gmail.com>
2024-12-07 18:28:31 +0700

    Subtraction-Bijar

--- main.c
+++ main.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 
-float add(float x, float y)
-{
-    return x + y;
+float subtract(float x, float y) {
+    return x - y;
 }
 
 int main()
 {
     float a, b, sum;
 
-    printf("Addition Calculator\n");
+    printf("Subtraction Calculator\n");
     printf("Enter two numbers: ");
     scanf("%f %f", &a, &b);
 
-    sum = add(a, b);
+    sum = subtract(a, b);
     printf("Result: %.2f\n", sum);
 
     return 0;
